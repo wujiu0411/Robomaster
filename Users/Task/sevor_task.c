9ca62a4a395a7aa027b5b7b26b63f3b846c6eccb
俊豪 胡 <2833957964@qq.com>
2024-09-23 13:44:03 +0800

    [feat,run]button按钮控制舵机正反转实现臂的抬升

--- Users/Task/sevor_task.c
+++ Users/Task/sevor_task.c
@@ -10,18 +10,22 @@ void StartSevorTask(void const * argument)
     uint32_t time = xTaskGetTickCount();
     extern uint8_t data[11];
     const remote_t *r=get_remote_control_point();
-
+    int Angle=0;
     HAL_TIM_PWM_Start(&htim1,TIM_CHANNEL_2);
-
+    
     while(1)
     {
         if(r->Button[0])
         {
-            
+            __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2, 90/180*2000+500);
+            HAL_Delay(1000);
         }
-        __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2, 50);
-        osDelayUntil(&time, 1000);
-        __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2, 250);
-			osDelayUntil(&time, 1000);
+        if(r->Button[1])
+        {
+            __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2, 180/180*2000+500);
+             HAL_Delay(1000);
+        }
+
+
     }
 }
