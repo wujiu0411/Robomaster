3ff0c18784b93ce99f421a33c5d54151a4e825e4
俊豪 胡 <2833957964@qq.com>
2024-09-25 18:37:06 +0800

    [feat,run]通过读取按钮改变GPIO高低电平控制继电器实现风机开和关

--- Users/Task/sevor_task.c
+++ Users/Task/sevor_task.c
@@ -17,19 +17,31 @@ void StartSevorTask(void const * argument)
     {
         if(r->Button[0])
         {
-            __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2, 90/180*2000+500);
+            __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2, 0/180*2000+500);
             HAL_Delay(1000);
         }
         if(r->Button[1])
-        {
+        {   
             __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2, 180/180*2000+500);
              HAL_Delay(1000);
         }
 
-        /*   
-        __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2, Angle/180*2000+500);
+        if(r->Button[2])
+        {
+            HAL_GPIO_WritePin(FAN_GPIO_Port, FAN_Pin, GPIO_PIN_SET);//控制继电器开，启动风机
+            HAL_Delay(1000);
+        }
+
+        if(r->Button[3])
+        {   
+            HAL_GPIO_WritePin(FAN_GPIO_Port, FAN_Pin, GPIO_PIN_RESET);//控制继电器关，关闭风机
+             HAL_Delay(1000);
+        }
+
+       /* __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2, 0/180*2000+500);
         HAL_Delay(1000);
-        __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2, Angle/180*2000+500);
+        __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2, 180/180*2000+500);
         HAL_Delay(1000);*/
+
     }
 }
