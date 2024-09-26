88bf35e6946adf8e8ac8332ac07d2c5e6cad6e00
harleyyyyy <1931168896@qq.com>
2024-09-26 21:10:17 +0800

    [fix,to]修改底盘逻辑

--- Users/Task/motor_task.c
+++ Users/Task/motor_task.c
@@ -5,77 +5,64 @@
 #include "usart.h"
 #include "ble_remote.h"
 
-#define tread 2.65 //轮距
-#define wheelbase 2.20 //轴距
-
-/**
- * @brief 编码器初始值置回10000，用于速度计算
- * @author Hujunhao
- * @date 2024-9-23
- * @retval None
- */
-void reset_counter()
-{ 
-    __HAL_TIM_SetCounter(&htim2,10000);
-    __HAL_TIM_SetCounter(&htim3,10000);
-    __HAL_TIM_SetCounter(&htim4,10000);
-    __HAL_TIM_SetCounter(&htim5,10000);
-}
-
-
-void StartmotorTask(void const * argument)
+void StartmotorTask(void const *argument)
 {
-    uint32_t time = xTaskGetTickCount();
-    const remote_t *r=get_remote_control_point();
+  uint32_t time = xTaskGetTickCount();
+  float Vx, Vy, Vz, k;
+  int16_t run[4];
+  int16_t feedback[4];
+  k = 0.265 / 2 + 0.220 / 2;
+  const remote_t *r = get_remote_control_point();
+  for (int i = 0; i < 4; i++)
+  {
+    feedback[i] = 0;
+    run[i] = 0;
+  }
+  Motor motor[4];
 
-    float Vx = r->rocker[0].x_position;
-    float Vy = r->rocker[0].y_position;  
-    float Vz = r->rocker[0].angle;
-    float k=Vz*(tread/2+wheelbase/2);
+  Motor_Init(&motor[0], &htim2, &htim8, TIM_CHANNEL_1, AIN1_GPIO_Port, AIN1_Pin, AIN2_GPIO_Port, AIN2_Pin);
+  Motor_Init(&motor[1], &htim3, &htim8, TIM_CHANNEL_2, BIN1_GPIO_Port, BIN1_Pin, BIN2_GPIO_Port, BIN2_Pin);
+  Motor_Init(&motor[2], &htim4, &htim8, TIM_CHANNEL_3, CIN1_GPIO_Port, CIN1_Pin, CIN2_GPIO_Port, CIN2_Pin);
+  Motor_Init(&motor[3], &htim5, &htim8, TIM_CHANNEL_4, DIN1_GPIO_Port, DIN1_Pin, DIN2_GPIO_Port, DIN2_Pin);
 
-    Motor motor[4];
+  for (size_t i = 0; i < 4; i++)
+  {
+    motor[i].PidInit(&motor[i], PID_DELTA, 7200, 100, 4.5, 0.4, 0.4);
+  }
 
-    Motor_Init(&motor[0], &htim2, &htim8, TIM_CHANNEL_1, AIN1_GPIO_Port, AIN1_Pin, AIN2_GPIO_Port, AIN2_Pin);
-    Motor_Init(&motor[1], &htim3, &htim8, TIM_CHANNEL_2, BIN1_GPIO_Port, BIN1_Pin, BIN2_GPIO_Port, BIN2_Pin);
-    Motor_Init(&motor[2], &htim4, &htim8, TIM_CHANNEL_3, CIN1_GPIO_Port, CIN1_Pin, CIN2_GPIO_Port, CIN2_Pin);
-    Motor_Init(&motor[3], &htim5, &htim8, TIM_CHANNEL_4, DIN1_GPIO_Port, DIN1_Pin, DIN2_GPIO_Port, DIN2_Pin);
+  // __HAL_TIM_SetCounter(&htim2, 10000);
+  // __HAL_TIM_SetCounter(&htim3, 10000);
+  // __HAL_TIM_SetCounter(&htim4, 10000);
+  // __HAL_TIM_SetCounter(&htim5, 10000);
 
-    motor[0].PidInit(&motor[0], PID_DELTA, 20000-1,100,4.5,0.4,0.4);
-    motor[1].PidInit(&motor[1], PID_DELTA, 20000-1,100,4.5,0.4,0.4);
-    motor[2].PidInit(&motor[2], PID_DELTA, 20000-1,100,4.5,0.4,0.4);
-    motor[3].PidInit(&motor[3], PID_DELTA, 20000-1,100,4.5,0.4,0.4);
-
-    __HAL_TIM_SetCounter(&htim2,10000);
-    __HAL_TIM_SetCounter(&htim3,10000);
-    __HAL_TIM_SetCounter(&htim4,10000);
-    __HAL_TIM_SetCounter(&htim5,10000);
-    
-
-    while(1)
+  while (1)
+  {
+    Vx = r->rocker[0].x_position;
+    Vy = r->rocker[0].y_position;
+    Vz = r->rocker[0].angle;
+    motor[0].speed_set = Vx + Vy - Vz * k; // k是一个参数 达到最丝滑旋转效果就好
+    motor[1].speed_set = Vx - Vy - Vz * k;
+    motor[2].speed_set = Vx + Vy + Vz * k;
+    motor[3].speed_set = Vx - Vy + Vz * k;
+    for (size_t i = 0; i < 4; i++)
     {
-      Vx = r->rocker[0].x_position;
-      Vy = r->rocker[0].y_position;
-      Vz = r->rocker[0].angle;
-      k=Vz*(tread/2+wheelbase/2);
-
-      motor[0].speed_set = Vx + Vy - Vz * k;
-      motor[1].speed_set = Vx - Vy - Vz * k;
-      motor[2].speed_set = Vx + Vy + Vz * k;
-      motor[3].speed_set = Vx - Vy + Vz * k;
-
-     for (size_t i = 0; i < 4; i++) 
-      {
-        motor[i].Driver(&motor[i], motor[i].speed);
-        motor[i].EncoderUpdate(&motor[i]);
-        motor[i].SpeedGet(&motor[i]);
-        motor[i].Calc(&motor[i]);
-        motor[i].speed += motor[i].pid.out;
-        
-        osDelayUntil(&time, POLL_INTERVAL_MS);
-      }
-
-      osDelayUntil(&time,500);
-
+      motor[i].EncoderUpdate(&motor[i]);
+    }
+    osDelay(1);
+    for (size_t i = 0; i < 4; i++)
+    {
+      motor[i].SpeedGet(&motor[i]);
+      // feedback[i] = motor[i].speed;
     }
 
+    for (size_t i = 0; i < 4; i++)
+    {
+      motor[i].Calc(&motor[i]);
+      // motor[i].Driver(&motor[i], motor[i].pid.out);
+      motor[i].Driver(&motor[i], 7000);
+      // run[i] = PID_Calc(&motor[i].pid, feedback[i], chassis[i]);
+      // motor[i].Driver(&motor[i], 16000);
+    }
+    osDelayUntil(&time, 99);
+  }
 }
