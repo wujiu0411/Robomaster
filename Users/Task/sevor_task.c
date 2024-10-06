658229f3af9e39e5d3e7a799d614708c074ee3d2
俊豪 胡 <2833957964@qq.com>
2024-10-06 20:32:37 +0800

    [feat,run]通过switch控制风机开关

--- Users/Task/sevor_task.c
+++ Users/Task/sevor_task.c
@@ -25,18 +25,6 @@ void StartSevorTask(void const * argument)
             __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2, 180/180*2000+500);
              osDelay(1000);
         }
-        if(r->Button[2])
-        {
-            HAL_GPIO_WritePin(FAN_GPIO_Port, FAN_Pin, GPIO_PIN_SET);//控制继电器开，启动风机
-            osDelay(1000);
-        }
-
-        if(r->Button[3])
-        {   
-            HAL_GPIO_WritePin(FAN_GPIO_Port, FAN_Pin, GPIO_PIN_RESET);//控制继电器关，关闭风机
-            osDelay(1000);
-        }
-        
         if(r->Switch[0])
         {
             __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_1, 220);//球门开
@@ -47,9 +35,20 @@ void StartSevorTask(void const * argument)
             __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_1, 260);//球门关
              osDelay(1000);
         }
-       __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2, 220);
+                if(r->Switch[2])
+        {
+            HAL_GPIO_WritePin(FAN_GPIO_Port, FAN_Pin, GPIO_PIN_SET);//控制继电器开，启动风机
+            osDelay(1000);
+        }
+
+        if(r->Switch[3])
+        {   
+            HAL_GPIO_WritePin(FAN_GPIO_Port, FAN_Pin, GPIO_PIN_RESET);//控制继电器关，关闭风机
+            osDelay(1000);
+        }
+       __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2, 80);
         HAL_Delay(1000);
-        __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2,260);
+        __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2,220);
         HAL_Delay(1000);
     }
 		     osDelayUntil(&time,1);
