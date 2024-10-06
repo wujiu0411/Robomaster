4d4b4da4128bec74f28a58a5933b3756bb0a69c6
俊豪 胡 <2833957964@qq.com>
2024-10-06 20:14:53 +0800

    [feat,run]球门开关

--- Users/Task/sevor_task.c
+++ Users/Task/sevor_task.c
@@ -17,31 +17,40 @@ void StartSevorTask(void const * argument)
     {
         if(r->Button[0])
         {
-            __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2, 0/180*2000+500);
-            HAL_Delay(1000);
+            __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2, 45);
+            osDelay(1000);
         }
         if(r->Button[1])
         {   
             __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2, 180/180*2000+500);
-             HAL_Delay(1000);
+             osDelay(1000);
         }
-
         if(r->Button[2])
         {
             HAL_GPIO_WritePin(FAN_GPIO_Port, FAN_Pin, GPIO_PIN_SET);//控制继电器开，启动风机
-            HAL_Delay(1000);
+            osDelay(1000);
         }
 
         if(r->Button[3])
         {   
             HAL_GPIO_WritePin(FAN_GPIO_Port, FAN_Pin, GPIO_PIN_RESET);//控制继电器关，关闭风机
-             HAL_Delay(1000);
+            osDelay(1000);
         }
-
-       /* __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2, 0/180*2000+500);
+        
+        if(r->Switch[0])
+        {
+            __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_1, 220);//球门开
+            osDelay(1000);
+        }
+        if(r->Switch[1])
+        {   
+            __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_1, 260);//球门关
+             osDelay(1000);
+        }
+       __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2, 220);
         HAL_Delay(1000);
-        __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2, 180/180*2000+500);
-        HAL_Delay(1000);*/
-
+        __HAL_TIM_SetCompare(&htim1, TIM_CHANNEL_2,260);
+        HAL_Delay(1000);
     }
+		     osDelayUntil(&time,1);
 }
